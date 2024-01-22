91f0494c922c87b3744c21b9ae26d0660ccf43dd
VisonaN <paulovisona@gmail.com>
2024-01-22 14:23:08 -0300

    ...

--- desafio-01/src/components/TituloDinamico.js
+++ desafio-01/src/components/TituloDinamico.js
@@ -2,22 +2,22 @@ class TituloDinamico extends HTMLElement {
     constructor(){
         super();
 
-        const shadow = this.attachShadow({mode: "open"});
+        const shadow = this.attachShadow({ mode: "open" });
 
-        // Base componente
         const componentRoot = document.createElement("h1");
+
         componentRoot.textContent = this.getAttribute("titulo");
-        // Estilizar componente
+
         const style = document.createElement("style");
         style.textContent = `
-            h1 {
+            h1{
                 color: red;
             }
         `;
-        // Enviar pra shadow
-            shadow.appendChild(componentRoot);
-            shadow.appendChild(style);
+
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
     }
 }
-// Sempre que criar um custom element usar o '-' no nome, se não ele não vai funcionar
-customElements.define('titulo-dinamico', TituloDinamico);
+
+customElements.define("titulo-dinamico", TituloDinamico);
